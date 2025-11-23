5d1fab6027f830030b05e5b78671ff123b67a994
piradiusquared <kpan5121@gmail.com>
2025-11-23 11:28:17 +1000

    [printf] Added support for printing single digit decimals. Working on multi-digit values

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -13,7 +13,7 @@ static bool print(const char* data, size_t length) {
 }
 
 int printf(const char* restrict format, ...) {
-	va_list parameters;
+	va_list parameters; /* Get a variable number of args like printing strings, chars*/
 	va_start(parameters, format);
 
 	int written = 0;
@@ -21,36 +21,46 @@ int printf(const char* restrict format, ...) {
 	while (*format != '\0') {
 		size_t maxrem = INT_MAX - written;
 
+		/* Enter if current is not % or next in line is % specifier */
 		if (format[0] != '%' || format[1] == '%') {
 			if (format[0] == '%')
-				format++;
+				format++; /* Move pointer forward to next char*/
+			
 			size_t amount = 1;
 			while (format[amount] && format[amount] != '%')
-				amount++;
-			if (maxrem < amount) {
+				amount++; /* Move forward until find another % or end of input */
+			
+			if (maxrem < amount) { /* Buffer overflow (printing over MAXINT) */
 				// TODO: Set errno to EOVERFLOW.
 				return -1;
 			}
-			if (!print(format, amount))
+
+			if (!print(format, amount)) /* End of char to print*/
 				return -1;
-			format += amount;
+			
+			format += amount; /* Update pointer and amount printed*/
 			written += amount;
 			continue;
 		}
 
 		const char* format_begun_at = format++;
-
-		if (*format == 'c') {
+		
+		switch (*format) {
+		case 'c':
 			format++;
 			char c = (char) va_arg(parameters, int /* char promotes to int */);
 			if (!maxrem) {
 				// TODO: Set errno to EOVERFLOW.
 				return -1;
 			}
-			if (!print(&c, sizeof(c)))
+
+			if (!print(&c, sizeof(c))) /* Print single char*/
 				return -1;
+
 			written++;
-		} else if (*format == 's') {
+			break;
+
+		case 's':
 			format++;
 			const char* str = va_arg(parameters, const char*);
 			size_t len = strlen(str);
@@ -61,17 +71,32 @@ int printf(const char* restrict format, ...) {
 			if (!print(str, len))
 				return -1;
 			written += len;
-		} else {
+		
+			break;
+			
+		case 'd':
+			format++;
+			char d = (char) ('0' + va_arg(parameters, int));
+			if (!maxrem)
+				return (-1); /* TODO: set to EOVERFLOW */
+			
+			if (!print(&d, sizeof(d)))
+				return (-1);
+			
+			written++;
+			break;
+			
+		default:
 			format = format_begun_at;
-			size_t len = strlen(format);
-			if (maxrem < len) {
+			size_t length = strlen(format);
+			if (maxrem < length) {
 				// TODO: Set errno to EOVERFLOW.
 				return -1;
 			}
-			if (!print(format, len))
+			if (!print(format, length))
 				return -1;
-			written += len;
-			format += len;
+			written += length;
+			format += length;
 		}
 	}
 
